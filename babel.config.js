5cbbdabd752cc2b145c89d3175578af20504ae60
feihoa <rtillkriyt@gmail.com>
2025-02-19 15:03:20 +0300

    env

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,15 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [['babel-plugin-root-import', { rootPathSuffix: 'app/', rootPathPrefix: '@/' }]],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+        },
+      ],
+      ['babel-plugin-root-import', { rootPathSuffix: 'app/', rootPathPrefix: '@/' }],
+    ],
   };
 };
